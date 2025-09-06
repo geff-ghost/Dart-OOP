4c61471f11355ed6aadeb6bead1769f1909a4eaf
OdhiamboGeoffrey <geoffrey.ochieng.odhiambo03@gmail.com>
2025-09-06 04:39:11 +0300

    Attributes and Constructors

--- oop.dart
+++ oop.dart
@@ -1,3 +1,34 @@
+class Person {
+  String name;
+  int? age;
+  int? id;
+  String? address;
+
+  Person({
+    required this.name,
+    required this.age,
+    required this.id,
+    required this.address,
+  });
+
+  void display() {
+    print('Name is $name');
+    print('Age is $age');
+    print('Id is $id');
+    print('Address is $address');
+  }
+
+  String give_name() {
+    return name;
+  }
+}
+
 void main() {
-  print('Hello World');
+  Person p1 = Person(name: 'Jay', age: 34, id: 1, address: 'home@example');
+
+  p1.display();
+  
+  String name = p1.give_name();
+
+  print(name);
 }
