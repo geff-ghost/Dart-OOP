12d59aae2e4c7ff32f66212dcfedeee5393b1a07
OdhiamboGeoffrey <geoffrey.ochieng.odhiambo03@gmail.com>
2025-09-06 11:38:36 +0300

    More about Constructors and name Parameters

--- oop.dart
+++ oop.dart
@@ -1,21 +1,21 @@
 class Person {
   String name;
   int? age;
-  int? id;
+  int? id = null;
   String? address;
 
   Person({
     required this.name,
     required this.age,
-    required this.id,
     required this.address,
+    this.id,
   });
 
   void display() {
-    print('Name is $name');
-    print('Age is $age');
-    print('Id is $id');
-    print('Address is $address');
+    print('Name is ${this.name}');
+    print('Age is ${this.age}');
+    print('Id is ${this.id}');
+    print('Address is ${this.address}');
   }
 
   String give_name() {
@@ -23,12 +23,26 @@ class Person {
   }
 }
 
+class Car {
+  String? make;
+  double? price;
+  dynamic speed;
+
+  Car(this.make, this.price, [this.speed = 'N/A']);
+
+  void display() {
+    print('Price: $price M');
+    print('Make: ${this.make}');
+    print('Speed: ${this.speed}');
+  }
+}
+
 void main() {
-  Person p1 = Person(name: 'Jay', age: 34, id: 1, address: 'home@example');
+  //   Person p1 = Person(name: 'Jay', age: 34, id: 1, address: 'home@example');
 
-  p1.display();
-  
-  String name = p1.give_name();
+  //   Person p2 = Person(name: 'Miraa', age: 12, address: 'home@example');
 
-  print(name);
+  Car car = Car('GLE', 1.5, 200);
+
+  car.display();
 }
